7b09d8daa5e16bf5323a36bd9924453ed230c2a5
Alexsander Akers <me@a2.io>
2015-09-30 14:25:50 +0200

    Add project

---
+++ PassbookCollectionViewLayout/PassbookCollectionViewLayout.h
@@ -0,0 +1,19 @@
+//
+//  PassbookCollectionViewLayout.h
+//  PassbookCollectionViewLayout
+//
+//  Created by Alexsander Akers on 9/30/15.
+//  Copyright © 2015 Pandamonia LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PassbookCollectionViewLayout.
+FOUNDATION_EXPORT double PassbookCollectionViewLayoutVersionNumber;
+
+//! Project version string for PassbookCollectionViewLayout.
+FOUNDATION_EXPORT const unsigned char PassbookCollectionViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PassbookCollectionViewLayout/PublicHeader.h>
+
+
